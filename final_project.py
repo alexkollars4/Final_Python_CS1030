e447e869c63de3281853e3ae1475a4abadcec9f5
Alex Kollars <alexkollars4@gmail.com>
2025-11-17 13:39:10 -0700

    First Try

---
+++ final_project.py
@@ -0,0 +1,9 @@
+import random
+
+#Quiz Questions
+questions = [
+    { 'question': 'What is the derivative of x^2?',
+        'options': ['2x', 'x', 'x^2', '1'],
+        'answer': '2x'
+    }, 
+    
